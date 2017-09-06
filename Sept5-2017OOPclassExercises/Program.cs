9e1e9fda415c361195f8d56579ade472c8382427
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2017-09-06 17:53:28 -0400

    first commit

---
+++ Sept5-2017OOPclassExercises/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sept5_2017OOPclassExercises
+{
+    class Program
+
+        //something basic:
+        //() indicates that something is a method
+        //eg, Console.WriteLine() is a method
+        //eureka
+
+        //three objects, with states (descriptions) and behaviors
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
