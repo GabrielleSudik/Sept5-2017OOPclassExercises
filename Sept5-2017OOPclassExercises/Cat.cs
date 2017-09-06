c4abbd31a5a7059feb6e23ee60fd2ac6a88738ed
GabrielleSudik <gabrielle.b.sudik@gmail.com>
2017-09-06 19:34:51 -0400

    updated during class

---
+++ Sept5-2017OOPclassExercises/Cat.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sept5_2017OOPclassExercises
+{
+    class Cat
+    {
+        //these are fields. ie - descriptors
+        //every cat will have these fields
+        private string name;
+        private string furColor;
+        private int furLength;
+        private bool isMale;
+        private int age;
+        private bool isHungry;
+        //they are private so random schmucks don't have access to them.
+
+        //properties
+        //these are public
+        //allows code outside the class to access fields
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+            public string FurColor
+        {
+            get { return this.furColor; }
+            set { this.furColor = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            //we won't use "set" here
+            //not needed. sometimes you want set but no get.
+        }
+            //etc...
+
+        //constructor:
+            //this is the default constructor
+        public Cat()
+        {
+            this.name = "Mittens";
+            //if other names aren't set, the name will be Mittens
+        }
+
+            //this is an overloaded constructor:
+            //it needs parameters
+            //it doesn't need them for every field, tho
+            //you can have more, so long as they have parameters
+            //you'll need to specify the parameters when you create an instance using this constructor
+
+        public Cat(string name, string furColor, int furLength, int age, bool isMale)
+        {
+            this.name = name;
+            this.furColor = furColor;
+            this.furLength = furLength;
+            this.age = age;
+            this.isMale = isMale;
+
+            //the this.thing part refer to the fields.
+            //the other side of the = is passed from the parameters when a version of cat is created
+
+        }
+
+        public Cat(string name)
+            //so this constructor will need the name only when an instance is created
+        {
+            this.name = name;
+        }
+
+        public Cat(int furLength)
+        {
+            this.furLength = furLength;
+        }
+
+        //public Cat(string furColor) //here's a thing:
+        //    //because you already have one overloaded method with one string, 
+        //    //you can't have another one with just one string.
+        //    //you need two strings or a string and an int or whatever.
+        //    //it's because the program won't know which string you're dealing with
+        //    //this applies to any single type of variable
+        //{
+        //    this.furColor = furColor;
+        //}
+
+        //here is your own thought:
+        //maybe good practice to have the default constructor,
+        //and at least one other constructor will accept all parameters
+        //because that will allow the greatest flexibility
+        //although you'll have to define all parameters for each instance.
+
+        //some programs use constructors to acquire things we need vs things we want
+        //like, amazon needs your address. phone is optional.
+    }
+}
--- Sept5-2017OOPclassExercises/Program.cs
+++ Sept5-2017OOPclassExercises/Program.cs
@@ -13,10 +13,46 @@ namespace Sept5_2017OOPclassExercises
         //eg, Console.WriteLine() is a method
         //eureka
 
-        //three objects, with states (descriptions) and behaviors
+        //classes contain:
+        //fields (variables that belong to the class)
+        //properties (like fields but allow access to fields)
+        //methods (what they do, repeatable and reusable code)
+        //constructors
+        
+        //Cat mittens = new Cat();
+
+    //to create a new class: right click second tier down off to the right
+    //click add, scroll down, select class, name it.
+    //it'll be in a different tab
+
     {
         static void Main(string[] args)
         {
+            Cat cat1 = new Cat();  //uses default constructor
+            Cat cat2 = new Cat("Neko", "black", 1, 4, false); //using other constructor
+                                                              //hovering on () will show what you need to pass
+
+            Console.WriteLine(cat1.Name);
+            Console.WriteLine(cat2.Name);
+
+            cat1.Name = "Snowy"; //this changes the name
+            cat2.Name = "Lance"; //same
+
+            cat1.FurColor = "grey";
+
+            Console.WriteLine(cat1.Name);
+            Console.WriteLine(cat2.Name);
+
+            Console.WriteLine(cat1.FurColor);
+
+            Console.WriteLine(cat1.Age); //we will get 0 because we don't set it, so default is 0.
+            Console.WriteLine(cat2.Age);
+
+            Cat cat3 = new Sept5_2017OOPclassExercises.Cat("Billy");
+
+            Console.WriteLine(cat3.Name);
+
+            Console.ReadLine();
         }
     }
 }
